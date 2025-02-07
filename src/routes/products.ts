281dfd7380d93d082cc2fa5a379021fdac93260c
divin3circle <omutelema.silas21@students.dkut.ac.ke>
2025-02-08 00:22:30 +0300

    Add search route for products with query parameter support

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -18,4 +18,28 @@ router.post(
   }
 );
 
+//SEARCH
+router.get(
+  "/search/:query",
+  async (req: Request, res: Response, next: NextFunction) => {
+    const query = req.params.query;
+    try {
+      const productsFound = await Product.find({
+        $or: [
+          { name: { $regex: new RegExp(query, "i") } },
+          { description: { $regex: new RegExp(query, "i") } },
+          { catergory: { $regex: new RegExp(query, "i") } },
+        ],
+      });
+      if (productsFound.length === 0) {
+        res.status(404).json({ message: "No product was found!" });
+        return;
+      }
+      res.status(200).json(productsFound);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
 export default router;
