244dbe4815cbdfa68afbea07cc2c5fac19d3c8d1
divin3circle <omutelema.silas21@students.dkut.ac.ke>
2025-02-08 11:36:56 +0300

    Add update, delete, and fetch all user addresses routes with error handling

--- src/routes/addresses.ts
+++ src/routes/addresses.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response, NextFunction } from "express";
 import Address from "../models/Address";
+import { CustomError } from "../middleware/error";
 
 const router = express.Router();
 
@@ -11,6 +12,67 @@ router.post(
     try {
       const savedAddress = await newAddress.save();
       res.status(201).json(savedAddress);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+//UPDATE ADDRESS
+router.put(
+  "/update/:id",
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { id } = req.params;
+    try {
+      const updatedAddress = await Address.findByIdAndUpdate(
+        id,
+        {
+          $set: req.body,
+        },
+        { new: true }
+      );
+      if (!updatedAddress) {
+        res.status(404).json({ message: "Address not found" });
+        throw new CustomError(404, "Address not found");
+      }
+      res.status(200).json(updatedAddress);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+// DELETE ADDRESS
+router.delete(
+  "/delete/:id",
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { id } = req.params;
+    try {
+      const addressToDelet = await Address.findByIdAndDelete(id);
+      if (!addressToDelet) {
+        res.status(404).json({ message: "Address not found" });
+        throw new CustomError(404, "Address not found");
+      }
+      res.status(204).json({ message: "Address deleted successfully" });
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+// GET ALL USER ADDRESSES
+router.get(
+  "/all/:userId",
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { userId } = req.params;
+
+    try {
+      const allAddresses = await Address.find({ user: userId });
+      if (allAddresses.length === 0) {
+        res.status(404).json({ message: "No address found" });
+        throw new CustomError(404, "No address found");
+      }
+      res.status(200).json(allAddresses);
     } catch (error) {
       next(error);
     }
