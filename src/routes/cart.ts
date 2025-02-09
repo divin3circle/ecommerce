58e3a3b2591146fc380c16c1455bce0189736eb0
divin3circle <omutelema.silas21@students.dkut.ac.ke>
2025-02-09 13:11:58 +0300

    feat: optimize cart total price calculation using aggregation

--- src/routes/cart.ts
+++ src/routes/cart.ts
@@ -15,24 +15,54 @@ router.post("/add", async (req: Request, res: Response, next: NextFunction) => {
     const existingProductIndex = cart.products.findIndex(
       (item) => item.product.toString() === productId
     );
-    console.log(existingProductIndex);
     if (existingProductIndex !== -1) {
       cart.products[existingProductIndex].quantity += quantity;
     } else {
       console.log("Adding product to cart:", { product: productId, quantity });
       cart.products.push({ product: productId, quantity });
     }
-    const productsInCart = await Product.find({
-      _id: { $in: cart.products.map((item) => item.product) },
-    });
-    let totalPrice = 0;
-    productsInCart.forEach((product) => {
-      const cartItem = cart.products.find(
-        (item) => item.product.toString() === product._id.toString()
-      );
-      totalPrice += product.price * cartItem.quantity;
-    });
-    cart.totalPrice = totalPrice;
+    // const productsInCart = await Product.find({
+    //   _id: { $in: cart.products.map((item) => item.product) },
+    // });
+    // let totalPrice = 0;
+    // productsInCart.forEach((product) => {
+    //   const cartItem = cart.products.find(
+    //     (item) => item.product.toString() === product._id.toString()
+    //   );
+    //   totalPrice += product.price * cartItem.quantity;
+    // });
+    // cart.totalPrice = totalPrice;
+
+    // Using aggregation to calculate total price
+    const cartWithTotalPrice = await Cart.aggregate([
+      { $match: { _id: cart._id } },
+      { $unwind: "$products" },
+      {
+        $lookup: {
+          from: "products",
+          localField: "products.product",
+          foreignField: "_id",
+          as: "productDetails",
+        },
+      },
+      { $unwind: "$productDetails" },
+      {
+        $group: {
+          _id: "$_id",
+          totalPrice: {
+            $sum: {
+              $multiply: ["$products.quantity", "$productDetails.price"],
+            },
+          },
+          products: { $push: "$products" },
+        },
+      },
+    ]);
+
+    if (cartWithTotalPrice.length > 0) {
+      cart.totalPrice = cartWithTotalPrice[0].totalPrice;
+      cart.products = cartWithTotalPrice[0].products;
+    }
     const savedCart = await cart.save();
     res.status(201).json({ message: "Item added to cart!", cart: savedCart });
   } catch (error) {
