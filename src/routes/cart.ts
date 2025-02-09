89ae9c4977428d8067df6529cd10a76ca29d3782
divin3circle <omutelema.silas21@students.dkut.ac.ke>
2025-02-09 13:29:09 +0300

    feat: implement remove product from cart functionality

--- src/routes/cart.ts
+++ src/routes/cart.ts
@@ -21,53 +21,83 @@ router.post("/add", async (req: Request, res: Response, next: NextFunction) => {
       console.log("Adding product to cart:", { product: productId, quantity });
       cart.products.push({ product: productId, quantity });
     }
-    // const productsInCart = await Product.find({
-    //   _id: { $in: cart.products.map((item) => item.product) },
-    // });
-    // let totalPrice = 0;
-    // productsInCart.forEach((product) => {
-    //   const cartItem = cart.products.find(
-    //     (item) => item.product.toString() === product._id.toString()
-    //   );
-    //   totalPrice += product.price * cartItem.quantity;
-    // });
-    // cart.totalPrice = totalPrice;
+    const productsInCart = await Product.find({
+      _id: { $in: cart.products.map((item) => item.product) },
+    });
+    let totalPrice = 0;
+    if (productsInCart.length > 0) {
+      productsInCart.forEach((product) => {
+        const cartItem = cart.products.find(
+          (item) => item.product.toString() === product._id.toString()
+        );
+        totalPrice += product.price * cartItem.quantity;
+      });
+    }
+    cart.totalPrice = totalPrice;
 
     // Using aggregation to calculate total price
-    const cartWithTotalPrice = await Cart.aggregate([
-      { $match: { _id: cart._id } },
-      { $unwind: "$products" },
-      {
-        $lookup: {
-          from: "products",
-          localField: "products.product",
-          foreignField: "_id",
-          as: "productDetails",
-        },
-      },
-      { $unwind: "$productDetails" },
-      {
-        $group: {
-          _id: "$_id",
-          totalPrice: {
-            $sum: {
-              $multiply: ["$products.quantity", "$productDetails.price"],
-            },
-          },
-          products: { $push: "$products" },
-        },
-      },
-    ]);
+    // const cartWithTotalPrice = await Cart.aggregate([
+    //   { $match: { _id: cart._id } },
+    //   { $unwind: "$products" },
+    //   {
+    //     $lookup: {
+    //       from: "products",
+    //       localField: "products.product",
+    //       foreignField: "_id",
+    //       as: "productDetails",
+    //     },
+    //   },
+    //   { $unwind: "$productDetails" },
+    //   {
+    //     $group: {
+    //       _id: "$_id",
+    //       totalPrice: {
+    //         $sum: {
+    //           $multiply: ["$products.quantity", "$productDetails.price"],
+    //         },
+    //       },
+    //       products: { $push: "$products" },
+    //     },
+    //   },
+    // ]);
 
-    if (cartWithTotalPrice.length > 0) {
-      cart.totalPrice = cartWithTotalPrice[0].totalPrice;
-      cart.products = cartWithTotalPrice[0].products;
-    }
+    // if (cartWithTotalPrice.length > 0) {
+    //   cart.totalPrice = cartWithTotalPrice[0].totalPrice;
+    //   cart.products = cartWithTotalPrice[0].products;
+    // }
     const savedCart = await cart.save();
     res.status(201).json({ message: "Item added to cart!", cart: savedCart });
   } catch (error) {
     next(error);
   }
 });
+
+// REMOVE PRODUCT FROM CART
+router.post(
+  "/remove",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { userId, productId } = req.body;
+      const cart = await Cart.findOne({ user: userId });
+      if (!cart) {
+        res.status(404).json({ message: "Cart not found" });
+        throw new Error("Cart not found");
+      }
+      const updatedCart = cart.products.filter(
+        (item) => item.product.toString() !== productId
+      );
+      if (updatedCart.length === 0) {
+        cart.totalPrice = 0;
+      }
+      cart.products = updatedCart;
+      const savedCart = await cart.save();
+      res
+        .status(200)
+        .json({ message: "Product removed from cart", cart: savedCart });
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
 export default router;
